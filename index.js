a420c0602cf540304fc112fb7a3572c61fcf4ffa
Harshika Maheswaran <85213206+harsh-csbs-2020-cit@users.noreply.github.com>
2023-03-10 20:45:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const fs = require("fs");
 const salt = bcrypt.genSaltSync(10);
 const secret = "gjyxtytytruytoobyrtexherxhrdfyjg";
 
-app.use(cors({ credentials: true, origin: "http://localhost:3001" }));
+app.use(cors({ credentials: true, origin: "https://bloggy-frontend-iys0.onrender.com/" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
