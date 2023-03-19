cb55e0960ccfa52bebc3ebdc27956736f459743d
Harshika Maheswaran <85213206+harsh-csbs-2020-cit@users.noreply.github.com>
2023-03-19 11:36:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,23 +14,23 @@ const fs = require("fs");
 const salt = bcrypt.genSaltSync(10);
 const secret = "gjyxtytytruytoobyrtexherxhrdfyjg";
 
-//app.use(cors({ credentials: true, origin: "https://bloggy-frontend-iys0.onrender.com/" }));
-app.use(cors());
+app.use(cors({ credentials: true, origin: "https://bloggy-frontend-iys0.onrender.com" }));
+// app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
 
-app.use((req, res, next) => {
+// app.use((req, res, next) => {
   
-  res.setHeader('Access-Control-Allow-Origin', 'https://bloggy-frontend-iys0.onrender.com');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+//   res.setHeader('Access-Control-Allow-Origin', 'https://bloggy-frontend-iys0.onrender.com');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   next();
+// });
 
 mongoose.connect(
   "mongodb+srv://Harshika:Star123@cluster0.nujrsoa.mongodb.net/?retryWrites=true&w=majority"
