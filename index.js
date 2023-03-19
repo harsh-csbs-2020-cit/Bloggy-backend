07506e22bb5b0403628ccbe075cd126142a92679
Harshika Maheswaran <85213206+harsh-csbs-2020-cit@users.noreply.github.com>
2023-03-19 11:17:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,10 @@ app.use((req, res, next) => {
   
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
 });
